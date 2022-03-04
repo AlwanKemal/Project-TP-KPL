9c28d5d20a2641867871e9e4b08fb5b698a17622
Kemaru345 <alwankemal@student.telkomuniversity.ac.id>
2022-03-04 07:49:11 +0700

    Project-TP-KPL

---
+++ TP_MOD2_1302204079/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TP_MOD2_1302204079
+{
+    partial class Test
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_submit = new System.Windows.Forms.Button();
+            this.label_input = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_submit
+            // 
+            this.button_submit.Location = new System.Drawing.Point(346, 207);
+            this.button_submit.Name = "button_submit";
+            this.button_submit.Size = new System.Drawing.Size(82, 25);
+            this.button_submit.TabIndex = 0;
+            this.button_submit.Text = "Submit";
+            this.button_submit.UseVisualStyleBackColor = true;
+            this.button_submit.Click += new System.EventHandler(this.button_submit_Click);
+            // 
+            // label_input
+            // 
+            this.label_input.AutoSize = true;
+            this.label_input.Location = new System.Drawing.Point(321, 84);
+            this.label_input.Name = "label_input";
+            this.label_input.Size = new System.Drawing.Size(146, 13);
+            this.label_input.TabIndex = 1;
+            this.label_input.Text = "Masukkan Nama Mahasiswa :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(288, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label_input);
+            this.Controls.Add(this.button_submit);
+            this.Name = "Test";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_submit;
+        private System.Windows.Forms.Label label_input;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ TP_MOD2_1302204079/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP_MOD2_1302204079
+{
+    public partial class Test : Form
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void button_submit_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            MessageBox.Show("Halo" + name);
+        }
+    }
+}
